566b546bf5a0b07f8015041c583e110df0f1c4de
David Cequea <dcequea@technotypes.com>
2025-12-02 18:35:47 -0400

    test vite github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,20 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+export default defineConfig(() => {
+    // const env = loadEnv(mode, '.', '');
     return {
-      base: '/username.github.io',
+      plugins: [react()],
+      base: '/username.github.io/',
       // server: {
       //   port: 443,
       //   host: '0.0.0.0',
       // },
-      plugins: [react()],
-      define: {},
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
+      // define: {},
+      // resolve: {
+      //   alias: {
+      //     '@': path.resolve(__dirname, '.'),
+      //   }
+      // }
     };
 });
